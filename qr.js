022b839610e396420c2d07272e50b485bf5d8c31
ishanxmd <ishanmadusanke548@gmail.com>
2026-02-20 19:35:04 +0530

    Fix caption formatting in qr.js

--- qr.js
+++ qr.js
@@ -145,7 +145,7 @@ router.get("/", async (req, res) => {
                             if (userJid) {
                                 await KnightBot.sendMessage(userJid, {
                                 image: { url: "https://files.catbox.moe/jt7099.png" },
-                                caption: `╔════════════════════◇
+                                caption: "╔════════════════════◇
 ║ 『 SESSION CONNECTED 』
 ║ ✨ ISHAN-X-MD BETA 🔷
 ║ ✨ ISHAN-X MD OFFICIAL 🔷
@@ -172,7 +172,7 @@ router.get("/", async (req, res) => {
 ║❍ 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: https://www.instagram.com/ishanmadusankerathnayake?igsh=MTE1aGVnOG80cWdjMQ==
 ║ ☬ ☬ ☬ ☬
 ╚═════════════════════╝
-𒂀 Enjoy ISHAN-X MD BETA`,
+𒂀 Enjoy ISHAN-X MD BETA",
                             });
                                 
                                 await KnightBot.sendMessage(userJid, {
